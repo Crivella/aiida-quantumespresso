da620ae7852bb644070dd1c5948af2dd99e1f1cb
Crivella <davidecrivella@yahoo.it>
2020-05-20 18:43:13 +0200

    Finished adding Bandsx calculation plugin

--- aiida_quantumespresso/calculations/bandsx.py
+++ aiida_quantumespresso/calculations/bandsx.py
@@ -9,34 +9,41 @@ from aiida.common import datastructures
 from aiida_quantumespresso.calculations.namelists import NamelistsCalculation
 
 
-class Pw2gwCalculation(NamelistsCalculation):
+class BandsxCalculation(NamelistsCalculation):
     """`CalcJob` implementation for the pw2gw.x code of Quantum ESPRESSO."""
 
     _INPUT_PSEUDOFOLDER = './pseudo/'
 
-    _EPS_X = 'epsX.dat'
-    _EPS_Y = 'epsY.dat'
-    _EPS_Z = 'epsZ.dat'
-    _EPS_TOT = 'epsTOT.dat'
+    _FILP = 'm_elems'
+    _FILBAND = 'filband'
+    _GNU = '{}.gnu'.format(_FILBAND)
+    _RAP = '{}.rap'.format(_FILBAND)
+    _PAR = '{}.par'.format(_FILBAND)
+    _SPIN1 = '{}.1'.format(_FILBAND)
+    _SPIN2 = '{}.2'.format(_FILBAND)
+    _SPIN3 = '{}.3'.format(_FILBAND)
 
-    _default_namelists = ['INPUTPP']
-    _internal_retrieve_list = [_EPS_X, _EPS_Y, _EPS_Z, _EPS_TOT]
+
+    _default_namelists = ['BANDS']
+    _internal_retrieve_list = [_FILBAND, _FILP, _GNU, _RAP, _PAR, _SPIN1, _SPIN2, _SPIN3]
     _blocked_keywords = [
-        ('INPUTPP', 'outdir', NamelistsCalculation._OUTPUT_SUBFOLDER),
-        ('INPUTPP', 'prefix', NamelistsCalculation._PREFIX),
+        ('BANDS', 'outdir', NamelistsCalculation._OUTPUT_SUBFOLDER),
+        ('BANDS', 'prefix', NamelistsCalculation._PREFIX),
+        ('BANDS', 'filband', _FILBAND),
+        ('BANDS', 'filp', _FILP),
     ]
 
     @classmethod
     def define(cls, spec):
         # yapf: disable
-        super(Pw2gwCalculation, cls).define(spec)
+        super(BandsxCalculation, cls).define(spec)
         spec.input('parent_folder', valid_type=orm.RemoteData,
             help='Output folder of a completed `PwCalculation`')
 
         spec.output('output_parameters', valid_type=orm.Dict,
             help='The `output_parameters` output node of the successful calculation.`')
-        spec.output('eps', valid_type=orm.ArrayData,
-            help='The `eps` output node containing 5 arrays `energy`, `epsX`, `epsY`, `epsZ`, `epsTOT`'
+        spec.output('filband', valid_type=orm.ArrayData,
+            help='The `filband` output node containing the arrays with the output data: `kpt`, `bands`. optinal:`lsigma1-3`, `rap`, `par`'
             )
 
         spec.exit_code(300, 'ERROR_NO_RETRIEVED_FOLDER',
@@ -51,16 +58,16 @@ class Pw2gwCalculation(NamelistsCalculation):
             message='The stdout output file could not be parsed.')
         spec.exit_code(312, 'ERROR_OUTPUT_STDOUT_INCOMPLETE',
             message='The stdout output file was incomplete probably because the calculation got interrupted.')
-        spec.exit_code(330, 'ERROR_OUTPUT_FILES_INVALID_FORMAT',
-            message='The eps*.dat output files do not have the expected shape (N, 2).')
-        spec.exit_code(331, 'ERROR_OUTPUT_FILES_ENERGY_MISMATCH',
-            message='The eps*.dat output files contains different values of energies.')
+        # spec.exit_code(330, 'ERROR_OUTPUT_FILES_INVALID_FORMAT',
+        #     message='The eps*.dat output files do not have the expected shape (N, 2).')
+        # spec.exit_code(331, 'ERROR_OUTPUT_FILES_ENERGY_MISMATCH',
+        #     message='The eps*.dat output files contains different values of energies.')
         spec.exit_code(350, 'ERROR_UNEXPECTED_PARSER_EXCEPTION',
             message='The parser raised an unexpected exception.')
 
     def prepare_for_submission(self, folder):
         # yapf: disable
-        calcinfo = super(Pw2gwCalculation, self).prepare_for_submission(folder)
+        calcinfo = super(BandsxCalculation, self).prepare_for_submission(folder)
 
         calcinfo.codes_run_mode = datastructures.CodeRunMode.SERIAL
 
